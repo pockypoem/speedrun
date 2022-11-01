a086a85307655e848ba747f6e99c1c17099a9ce9
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-01 12:34:49 +0700

    Map through State Array

--- react101/speed-react/hellow-react/src/App.js
+++ react101/speed-react/hellow-react/src/App.js
@@ -1,9 +1,51 @@
 import './index.css';
 import Employee from './components/Employee';
 import { useState } from 'react';
+import {v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [role, setRole] = useState('dev');
+  const [employees, setEmployees] = useState(
+    // Array of multiple employees
+    [
+      {
+        id: 1,
+        name: "Pocky", 
+        role: "Developer", 
+        img: "https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"
+      },
+      {
+        id: 2,
+        name: "Sal", 
+        role: "Manager", 
+        img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg"
+      },
+      {
+        id: 3,
+        name: "John", 
+        role: "Director of Eng.", 
+        img: "https://images.pexels.com/photos/2095582/pexels-photo-2095582.jpeg"
+      },
+      {
+        id: 4,
+        name: "Melanie", 
+        role: "Software Engineer", 
+        img: "https://images.pexels.com/photos/3760583/pexels-photo-3760583.jpeg"
+      },
+      {
+        id: 5,
+        name: "Corey", 
+        role: "DevOps", 
+        img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg"
+      },
+      {
+        id: 6,
+        name: "Jake", 
+        role: "Semior Intern", 
+        img: "https://images.pexels.com/photos/2225298/pexels-photo-2225298.jpeg"
+      }
+    ]
+  );
   const showEmployees = true;
   return (
     <div className="App">
@@ -14,15 +56,20 @@ function App() {
             setRole(e.target.value);
           }}/>
           <div className='flex flex-wrap justify-center'>
-            <Employee name="Pocky" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="Pocky" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="Pocky" role="Intern" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="Abby" role={role} img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
-            <Employee name="John" img="https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg"/>
+            {/* Loop through our state data */}
+            {employees.map((employee) => {
+              console.log(uuidv4());
+              return(
+                <Employee 
+                  key= {uuidv4()}
+                  name={employee.name} 
+                  role={employee.role} 
+                  img={employee.img}
+
+                />
+              )
+              
+            })}
           </div>
         </>
       :
