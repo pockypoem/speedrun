991b4a24e06b9268b1c443513b699d57c24a8db8
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-01 11:58:13 +0700

    Styling React with Tailwind

--- react101/speed-react/hellow-react/src/components/Employee.js
+++ react101/speed-react/hellow-react/src/components/Employee.js
@@ -2,7 +2,7 @@ function Employee(props) {
     return (
 
         <div class="m-2 py-8 px-8 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src={props.img}/>
+            <img class="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0" src={props.img}/>
             <div class="text-center space-y-2 sm:text-left">
                 <div class="space-y-0.5">
                     <p class="text-lg text-black font-semibold">
