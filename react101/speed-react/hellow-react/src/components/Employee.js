e9c2a389c7a5eef664d8c7564ec921b837a7cd86
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-02 18:22:58 +0700

    fixing bug

--- react101/speed-react/hellow-react/src/components/Employee.js
+++ react101/speed-react/hellow-react/src/components/Employee.js
@@ -4,7 +4,7 @@ function Employee(props) {
     return (
 
         <div className="min-w-[350px] max-w-[350px] m-2 py-8 px-8 bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0" src={props.img}/>
+            <img className="object-cover rounded-full h-[100px] w-[100px] block mx-auto sm:mx-0 sm:shrink-0" src={props.img} alt="profile-img"/>
             <div className="text-center space-y-2 sm:text-left">
                 <div className="space-y-0.5">
                     <p className="text-lg text-black font-semibold">
