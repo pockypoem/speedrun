cd1e15009007caa898d9d3bc3524abd785b95057
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-10 13:05:26 +0700

    constructor __init__
    __init__ : akan dieksekusi ketika object pertama kali dibuat

--- oopy/01-intro/main.py
+++ oopy/01-intro/main.py
@@ -1,20 +1,16 @@
-class Hero: # template
-    pass
+class Hero:  # template
+
+    def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        self.name = inputName
+        self.health = inputHealth
+        self.power = inputPower
+        self.armor = inputArmor
 
 
-hero1 = Hero() #  object / instance (instansiate)
-hero2 = Hero()
-hero3 = Hero()
+hero1 = Hero("sniper", 100, 10, 4)
+hero2 = Hero("mirana", 100, 15, 1)
+hero3 = Hero("Pocky", 1000, 100, 0)
 
-hero1.name = "Sniper"
-hero1.health = 100
-
-hero2.name = "Sven"
-hero2.health = 200
-
-hero3.name = "Pocky"
-hero3.health = 1000
-
-print(hero1)
 print(hero1.__dict__)
-print(hero1.name)
+print(hero2.__dict__)
+print(hero3.__dict__)
