81eec661c2fb7eefbae4c710654818a2ce2f849b
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-10 13:14:45 +0700

    Class and Instance Variables

--- oopy/01-intro/main.py
+++ oopy/01-intro/main.py
@@ -1,16 +1,20 @@
 class Hero:  # template
+    # class variabel
+    jumlah = 0
 
     def __init__(self, inputName, inputHealth, inputPower, inputArmor):
+        # Instance variable
         self.name = inputName
         self.health = inputHealth
         self.power = inputPower
         self.armor = inputArmor
+        Hero.jumlah += 1
+        print("Membuat Hero dengan nama " + inputName)
 
 
 hero1 = Hero("sniper", 100, 10, 4)
+print(Hero.jumlah)
 hero2 = Hero("mirana", 100, 15, 1)
+print(Hero.jumlah)
 hero3 = Hero("Pocky", 1000, 100, 0)
-
-print(hero1.__dict__)
-print(hero2.__dict__)
-print(hero3.__dict__)
+print(Hero.jumlah)
