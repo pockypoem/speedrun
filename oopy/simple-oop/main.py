715d962b8cd54263a550840209a3732b7a4e0f71
Jeremyas Cornelis <jeremyas.cornelis@gmail.com>
2022-11-11 12:00:23 +0700

    simple oop practice

--- oopy/simple-oop/main.py
+++ oopy/simple-oop/main.py
@@ -6,14 +6,21 @@ class Hero:
         self.attackPower = attackPower
         self.armorNumber = armorNumber
 
-    def serang(self):
-        print(self.name + " menyerang")
+    def serang(self, lawan):
+        print(self.name + " menyerang " + lawan.name)
+        lawan.diserang(self, self.attackPower)
 
-    def diserang(self):
-        print(self.name + " diserang")
+    def diserang(self, lawan, attackPower_lawan):
+        print(self.name + " diserang " + lawan.name)
+        attack_diterima = attackPower_lawan / self.armorNumber
+        print('serangan terasa: ' + str(attack_diterima))
+        self.health -= attack_diterima
+        print("darah " + self.name + ' tersisa ' + str(self.health))
 
 
 sniper = Hero("sniper", 100, 10, 5)
-rikimaru = Hero("rikimaru", 100, 5, 10)
+rikimaru = Hero("rikimaru", 100, 20, 10)
 
-sniper.serang()
+sniper.serang(rikimaru)
+print("\n")
+rikimaru.serang(sniper)
